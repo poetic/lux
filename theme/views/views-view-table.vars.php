03d2bfb3477048c51685e88ec8c92e4ff11793f6
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-04-10 21:38:42 -0500

    refactor/polish for table code and styling

--- theme/views/views-view-table.vars.php
+++ theme/views/views-view-table.vars.php
@@ -8,8 +8,7 @@
  * Implements hook_preprocess_views_view_table().
  */
 function lux_preprocess_views_view_table(&$vars) {
-  $vars['classes_array'][] = 'hoverable';
-  $count = 0;
+  $vars['classes_array'][] = 'hoverable card-panel grey lighten-5';
   foreach ($vars['rows'] as $num => $row) {
   	$vars['row_classes'][$num] = array();
   	$vars['row_classes'][$num][] = 'card-panel';
