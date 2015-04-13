0b6c0adcefda456ed6d1b98d8865395e0c9fd2e3
Matt Coker <matt.james.coker@gmail.com>
2015-04-12 23:10:38 -0500

    Add styling to primary/secondary navigation menus, commonly used for node editing links, added necessary javascript

--- js/lux.settings.js
+++ js/lux.settings.js
@@ -1,5 +1,7 @@
 (function(window, document, $){
 	$(document).ready(function() {
+
+    // Enable Materialize select
 		$('select').material_select();
 
     // File Input Path
@@ -9,6 +11,11 @@
         path_input.val($(this)[0].files[0].name);
         path_input.trigger('change');
       });
+    });
+
+    // Reenable primary link functionality
+    $('.primary a, .secondary a').on('click', function() {
+      window.location = $(this).attr('href');
     });
 	});
 })(window, document, jQuery);
