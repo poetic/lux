bf1fb4c7b15b90a25b35d6512855d7895d992a91
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-04-18 13:31:28 -0500

    tidy up template.php, add .func.php for file widget

--- template.php
+++ template.php
@@ -179,33 +179,6 @@ function lux_find_theme_includes($registry, $extension, $path) {
  */
 // lux_include('lux', 'theme/alter.inc');
 
-
-
-
-
-
-function lux_file_widget($variables) {
-  $element = $variables['element'];
-  $output = '';
-
-  // The "form-managed-file" class is required for proper Ajax functionality.
-  // $output .= '<div class="file-widget form-managed-file clearfix">';
-  $output .= '<div class="file-field input-field">';
-  $output .= '<input class="file-path validate" type="text"/>';
-  $output .= '<div class="btn">';
-  $output .= '<span>File</span>';
-  $output .= '</div>';
-
-  if ($element['fid']['#value'] != 0) {
-    // Add the file size after the file name.
-    $element['filename']['#markup'] .= ' <span class="file-size">(' . format_size($element['#file']->filesize) . ')</span> ';
-  }
-  $output .= drupal_render_children($element);
-  $output .= '</div>';
-
-  return $output;
-}
-
 function lux_menu_local_task($variables) {
   $link = $variables ['element']['#link'];
   $link_text = $link ['title'];
